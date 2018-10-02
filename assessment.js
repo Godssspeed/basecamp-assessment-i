2aa0b36f48d5a0436a1b5112117e6b3dcd79cc3a
Godssspeed <33368773+Godssspeed@users.noreply.github.com>
2018-10-01 23:39:51 -0500

    Add files via upload
    Updated Tests

--- assessment.js
+++ assessment.js
@@ -16,11 +16,13 @@ var me = "My name is " + myName + " and my favorite number is " + faveNum;
 
 // #5 Using the previously created faveNum variable, create a new variable called 'trueFaveNum'. *trueFaveNum* should equal true when comparing faveNum to your favorite number.v // HINT: Use a comparison operator
 
-var trueFaveNum = faveNum >= 4;
+var trueFaveNum = faveNum;
 
 // #6 Create a new variable called notHakeem. Using the previously created myName variable, test that your name DOES NOT equal the string 'Hakeem Olajuwon'. If your name does happen to be Hakeem Olajuwon, test against 'Karl Malone'
 
-var notHakeem = myName != "Hakeem Olajuwon";
+var notHakeem = myName;
+
+myName = "Hakeem Olajuwon" ? console.log(`My name is ${myName}`) : console.log(`My name is ${notHakeem}`);
 
 // #7 Create an if statement that checks the age variable below. If age is greater than 16, then set canDrive to true otherwise set canDrive to false. The canDrive variable will be set by the unit test so do not adjust it.
 var age = 25;
@@ -28,13 +30,7 @@ var canDrive = 16;
 
 function eligibleDriver (){
 
-if(age >= canDrive ){
-  console.log(true);
-}
-
-else {
-  console.log(false)
-}
+age >= canDrive ? console.log(true) : console.log(false);
 
 };
 
@@ -64,7 +60,8 @@ function sum (param){
 // #10 Create a function called 'greeter' that takes in two parameters, name and number. Have 'greeter' return a string that says something like this 'Hi! I am Bryan and I am 28 years old" but with the appropriate values..
 
 function greeter (name, num){
-  return "Hi! I am " + name + " and I am " + num + " years old"
+  return `Hi! I am ${name} and I am ${num} years old`
+
 }
 
 
@@ -75,7 +72,7 @@ function greeter (name, num){
 console.log(me);
 console.log(trueFaveNum);
 console.log(notHakeem);
-console.log(eligibleDriver());
+eligibleDriver();
 
 console.log(responseCreator("fefenf"));
 
